d8c9c2bac04eefe7773d72c3d795c901fedb9f97
vl222cu <vl222cu@student.lnu.se>
2014-03-08 20:02:00 +0100

    DepartmentDAL klar förhoppningsvis

--- Projekt_1dv406/Projekt_1dv406/Model/DAL/DepartmentDAL.cs
+++ Projekt_1dv406/Projekt_1dv406/Model/DAL/DepartmentDAL.cs
@@ -1,11 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 
 namespace Projekt_1dv406.Model.DAL
 {
-    public class DepartmentDAL
+    public class DepartmentDAL : DALBase
     {
+        // Hämtar alla avdelningar i databasen från tabellen avdelning
+        public static IEnumerable<Department> GetDepartments()
+        {
+            using (var conn = CreateConnection())
+            {
+                try
+                {
+                    var departments = new List<Department>(10);
+
+                    var cmd = new SqlCommand("appSchema.GetDepartments", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    conn.Open();
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        var depIdIndex = reader.GetOrdinal("AvdID");
+                        var depIndex = reader.GetOrdinal("Avdelning");
+
+                        while (reader.Read())
+                        {
+                            departments.Add(new Department
+                            {
+                                AvdID = reader.GetInt32(depIdIndex),
+                                Avdelning = reader.GetString(depIndex), 
+                            });
+                        }
+                    }
+
+                    departments.TrimExcess();
+                    return departments;
+                }
+                catch (Exception)
+                {
+                    throw new ApplicationException("Ett fel uppstod vid kontakt med databasen.");
+                }
+            }
+        }
+
+        // Hämtar en avdelning i databasen från tabellen avdelning
+        public Department GetDepartment(Department department)
+        {
+            using (var conn = CreateConnection())
+            {
+                try
+                {
+                    var cmd = new SqlCommand("appSchema.GetDepartment", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add("@AvdID", SqlDbType.Int, 4).Value = department.AvdID;
+
+                    conn.Open();
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            var depIdIndex = reader.GetOrdinal("AvdID");
+                            var depIndex = reader.GetOrdinal("Avdelning");
+
+                            return new Department
+                            {
+                                AvdID = reader.GetInt32(depIdIndex),
+                                Avdelning = reader.GetString(depIndex)
+                            };
+                        }
+                    }
+
+                    return null;
+                }
+                catch (Exception)
+                {
+                    throw new ApplicationException("Ett fel uppstod vid kontakt med databasen.");
+                }
+            }
+        }
     }
 }
