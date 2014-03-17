1701e25ce9e0edb7c7c6dca1513e15901e40d84f
vl222cu <vl222cu@student.lnu.se>
2014-03-17 14:59:40 +0100

    Ändrat lite i RouteConfigen

--- Projekt_1dv406/Projekt_1dv406/App_Start/RouteConfig.cs
+++ Projekt_1dv406/Projekt_1dv406/App_Start/RouteConfig.cs
@@ -10,29 +10,17 @@ namespace Projekt_1dv406
     {
         public static void LinkRoutes(RouteCollection routes)
         {
-            routes.MapPageRoute("Default",
-                "",
-                "~/Pages/CaseCreate.aspx");
+            routes.MapPageRoute("Default", "", "~/Pages/CaseListing.aspx");
 
-            routes.MapPageRoute("CaseCreate",
-                "felanmälan/ny",
-                "~/Pages/CaseCreate.aspx");
+            routes.MapPageRoute("CaseCreate", "felanmälan/ny", "~/Pages/CaseCreate.aspx");
 
-            routes.MapPageRoute("CaseListing",
-                "felanmälan/lista",
-                "~/Pages/CaseListing.aspx");
+            routes.MapPageRoute("CaseListing", "felanmälan/lista", "~/Pages/CaseListing.aspx");
 
-            routes.MapPageRoute("CaseAssignments",
-                "felanmälan/{id}",
-                "~/Pages/CaseAssignments.aspx");
+            routes.MapPageRoute("CaseAssignments", "felanmälan/{id}", "~/Pages/CaseAssignments.aspx");
 
-            routes.MapPageRoute("CaseEdit",
-                "felanmälan/{id}/redigera",
-                "~/Pages/CaseEdit.aspx");
+            routes.MapPageRoute("CaseEdit", "felanmälan/{id}/redigera", "~/Pages/CaseEdit.aspx");
 
-            routes.MapPageRoute("CaseDelete",
-                "felanmälan/{id}/radera",
-                "~/Pages/CaseDelete.aspx");
+            routes.MapPageRoute("CaseDelete", "felanmälan/{id}/radera", "~/Pages/CaseDelete.aspx");
         }
     }
 }
