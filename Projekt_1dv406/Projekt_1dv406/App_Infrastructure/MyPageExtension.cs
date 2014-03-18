f6dcca34956b169d491552e2b218200512b1ff58
vl222cu <vl222cu@student.lnu.se>
2014-03-18 04:23:47 +0100

    Lagt till MyPageExtension på nytt

---
+++ Projekt_1dv406/Projekt_1dv406/App_Infrastructure/MyPageExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+
+public static class MyPageExtension
+{
+    public static object GetTempData(this Page page, string key)
+    {
+        var value = page.Session[key];
+        page.Session.Remove(key);
+        return value;
+    }
+        
+    public static void SetTempData(this Page page, string key, object value)
+    {
+        page.Session[key] = value;
+    } 
+}
